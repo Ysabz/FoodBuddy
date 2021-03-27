04533d3323e2db881b56896aea3caeb0b34e60eb
Yasaman <Yasamanarya80@yahoo.com>
2021-03-26 20:20:56 -0400

    added health bar

---
+++ FoodBuddy/Assets/HealthBarController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image filled;
+
+
+    public void SetMaxHealth(int value)
+    {
+        slider.maxValue = value;
+        slider.value = value;
+        filled.color = gradient.Evaluate(1f);
+    }
+
+    public void SetHealth(int value)
+    {
+        slider.value = value;
+        filled.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
---
+++ FoodBuddy/Assets/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 20;
+    public int currentHealth;
+    public HealthBarController healthBar;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    public void Print(bool found)
+    {
+        if (found)
+        {
+           Debug.Log("Healthy food found");
+            currentHealth = currentHealth - 2;
+            healthBar.SetHealth(currentHealth - 2);
+        }
+
+        else
+        {
+            Debug.Log("Healthy food lost");
+            currentHealth = currentHealth - 2;
+            healthBar.SetHealth(currentHealth - 2);
+        }
+
+    }
+
+
+}
