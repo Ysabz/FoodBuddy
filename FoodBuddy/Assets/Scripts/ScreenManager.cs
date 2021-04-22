3610cb0143c83b508e1abdd755556ced0785721f
Yasaman <Yasamanarya80@yahoo.com>
2021-04-22 17:04:15 -0400

    UI beautifying

--- FoodBuddy/Assets/Scripts/ScreenManager.cs
+++ FoodBuddy/Assets/Scripts/ScreenManager.cs
@@ -12,7 +12,9 @@ public class ScreenManager : MonoBehaviour
     public Button red;
     public Button green;
     public Button yellow;
-    public Button healthyDrink;
+    public Button healthyDrink1;
+    public Button healthyDrink2;
+    public Button healthyDrink3;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +22,9 @@ public class ScreenManager : MonoBehaviour
         red.onClick.AddListener(() => OpenInfoWindow("Badge awarded for eating 10 red healthy foods during one month", "Healthy Red Champion"));
         green.onClick.AddListener(() => OpenInfoWindow("Badge awarded for eating 10 green healthy foods during one month", "Healthy Green Champion"));
         yellow.onClick.AddListener(() => OpenInfoWindow("Badge awarded for eating 10 yellow healthy foods during one month", "Healthy Yellow Champion"));
-        healthyDrink.onClick.AddListener(() => OpenInfoWindow("Badge awarded for drinking 10 yellow healthy drinks during one month", "Yellow Potion Champion"));
+        healthyDrink1.onClick.AddListener(() => OpenInfoWindow("Badge awarded for drinking 10 yellow healthy drinks during one month", "Yellow Potion Champion"));
+        healthyDrink2.onClick.AddListener(() => OpenInfoWindow("Badge awarded for drinking 10 green healthy drinks during one month", "Green Potion Champion"));
+        healthyDrink3.onClick.AddListener(() => OpenInfoWindow("Badge awarded for drinking 10 orange healthy drinks during one month", "Orange Potion Champion"));
     }
 
     public void OpenInfoWindow(string text, string title)
